e571552e050ac968ece75cef3ef4cd43cc0f98e3
NickKumar <Timerbob@gmail.com>
2022-04-14 14:35:33 -0600

    Further styling. Minigame added.

--- README.md
+++ README.md
@@ -4,4 +4,7 @@ Dice roller project
 13/04/2022
 My first project. I have spent way too long and been too fussy over little details when I wanted to practice layout and learn about buttons and input. So far I have learned grid, js animations, hilarious function mayhem, onclick and I struggled hard with the rotate animation as a function. It would only turn once and then stop. Next I want to tackle making my functions more universal instead of copy pasting them for each button. The center needs finishing and I need to learn setTimeout.
 
-A full day of changes. I updated the javascript so it wasn't repetitive. It was a nightmare to style css with variables. I believe i found a good example and it works. I struggled to add references to elements globally. It worked to place them at the bottom of the HTML page. I repositioned the "crystal ball", it looks awful and cheesy and I dont' believe it is the look I want. Next I want to design pull out menus for skills and a way to fold the log away out of sight.
+A full day of changes. I updated the javascript so it wasn't repetitive. It was a nightmare to style css with variables. I believe i found a good example and it works. I struggled to add references to elements globally. It worked to place them at the bottom of the HTML page. I repositioned the "crystal ball", it looks awful and cheesy and I don’t believe it is the look I want. Next I want to design pull out menus for skills and a way to fold the log away out of sight.
+
+14/04/2022
+More styling. I don’t think I’m learning anything new but I am learning to google solutions to my problems. I added a minigame. I don’t know why. I did learn setTimeout. I need to remove old useless css.
--- javascript.js
+++ javascript.js
@@ -2,10 +2,24 @@ let crit = "";
 let light = "";
 let dark = "";
 let roll_4 = "";
+let roll_6 = "";
+let roll_8 = "";
+let roll_10 = "";
+let roll_12 = "";
+let roll_20 = "";
+let roll_100 = "";
 let color = "";
 let rollX = "";
 let lastAngle = 1;
 let spinAmount = "";
+let lock4 = "";
+let lock6 = "";
+let lock8 = "";
+let lock10 = "";
+let lock12 = "";
+let lock20 = "";
+let lock100 = "";
+let clicks = 0;
 
 function d4_roll() {
   let roll_4 = Math.floor(Math.random() * 4) + 1;
@@ -23,9 +37,17 @@ function d4_roll() {
   color = "imgYellow";
   spinAmount = 360 / 4;
 
+  if (roll_4 == 4) {
+    lock4 = true;
+    console.log(lock4);
+  } else {
+    lock4 = false;
+  }
+
   spin();
   colorPop();
   setTimeout(removeZoom, 800);
+  unlock();
 }
 
 function d6_roll() {
@@ -44,9 +66,17 @@ function d6_roll() {
   color = "imgOrange";
   spinAmount = 360 / 6;
 
+  if (roll_6 == 6) {
+    lock6 = true;
+    console.log(lock6);
+  } else {
+    lock6 = false;
+  }
+
   spin();
   colorPop();
   setTimeout(removeZoom, 800);
+  unlock();
 }
 
 function d8_roll() {
@@ -65,9 +95,17 @@ function d8_roll() {
   color = "imgRed";
   spinAmount = 360 / 8;
 
+  if (roll_8 == 8) {
+    lock8 = true;
+    console.log(lock8);
+  } else {
+    lock8 = false;
+  }
+
   spin();
   colorPop();
   setTimeout(removeZoom, 800);
+  unlock();
 }
 
 function d10_roll() {
@@ -86,9 +124,17 @@ function d10_roll() {
   color = "imgBlue";
   spinAmount = 360 / 10;
 
+  if (roll_10 == 10) {
+    lock10 = true;
+    console.log(lock10);
+  } else {
+    lock10 = false;
+  }
+
   spin();
   colorPop();
   setTimeout(removeZoom, 800);
+  unlock();
 }
 
 function d12_roll() {
@@ -107,9 +153,17 @@ function d12_roll() {
   color = "imgIndigo";
   spinAmount = 360 / 12;
 
+  if (roll_12 == 12) {
+    lock12 = true;
+    console.log(lock12);
+  } else {
+    lock12 = false;
+  }
+
   spin();
   colorPop();
   setTimeout(removeZoom, 800);
+  unlock();
 }
 
 function d20_roll() {
@@ -128,9 +182,17 @@ function d20_roll() {
   color = "imgViolet";
   spinAmount = 360 / 20;
 
+  if (roll_20 == 20) {
+    lock20 = true;
+    console.log(lock20);
+  } else {
+    lock20 = false;
+  }
+
   spin();
   colorPop();
   setTimeout(removeZoom, 800);
+  unlock();
 }
 
 function d100_roll() {
@@ -149,9 +211,17 @@ function d100_roll() {
   color = "imgGreen";
   spinAmount = 360 / 100;
 
+  if (roll_100 == 100) {
+    lock100 = true;
+    console.log(lock100);
+  } else {
+    lock100 = false;
+  }
+
   spin();
   colorPop();
   setTimeout(removeZoom, 800);
+  unlock();
 }
 
 function colorPop() {
@@ -166,6 +236,21 @@ function colorPop() {
   }
 }
 
+function unlock() {
+  if (
+    lock4 &&
+    lock6 &&
+    lock8 &&
+    lock10 &&
+    lock12 &&
+    lock20 &&
+    lock100 == true
+  ) {
+    const element2 = document.getElementById("page2");
+    element2.classList.remove("hide");
+  }
+}
+
 function spin() {
   lastAngle += rollX * spinAmount;
   document.getElementById(color).style.transform =
@@ -176,7 +261,16 @@ function spin() {
 function removeZoom() {
   const span = document.getElementById("bigText");
   span.classList.remove("zoom");
-  console.log("clear");
+}
+
+function reset() {
+  const element3 = document.getElementById("page2");
+  element3.classList.add("hide");
+}
+
+function clickCounter() {
+  clicks += 1;
+  document.getElementById("clicks").innerHTML = clicks;
 }
 
 const red = "#B31F1B";
